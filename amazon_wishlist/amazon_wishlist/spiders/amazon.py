edaf699190c7bf0d26e8cfcc3aaa816efe6f2c15
kinari321 <abcdream321@yahoo.co.jp>
2023-09-04 22:50:21 +0900

    sample→book

--- amazon_wishlist/amazon_wishlist/spiders/amazon.py
+++ amazon_wishlist/amazon_wishlist/spiders/amazon.py
@@ -8,25 +8,26 @@ class AmazonSpider(scrapy.Spider):
     name = "amazon"
     allowed_domains = ["amazon.co.jp"]
     start_urls = [
-        "https://www.amazon.co.jp/%E3%82%A8%E3%82%AF%E3%82%B9%E3%83%88%E3%83%AA%E3%83%BC%E3%83%A0%E3%83%97%E3%83%AD%E3%82%B0%E3%83%A9%E3%83%9F%E3%83%B3%E3%82%B0-%E3%82%B1%E3%83%B3%E3%83%88-%E3%83%99%E3%83%83%E3%82%AF/dp/4274217620/"
+        "https://www.amazon.co.jp/hz/wishlist/ls/3VQQS89WB9IIA?ref_=list_d_wl_lfu_nav_7"
     ]
 
     def parse(self, response):
-        sample = AmazonWishlistItem()
+        book = AmazonWishlistItem()
 
-        sample["title"] = response.xpath('//*[@id="productTitle"]/text()').get()
-        sample["author"] = response.xpath(
+        book["title"] = response.xpath('//*[@id="productTitle"]/text()').get()
+        
+        book["author"] = response.xpath(
             '//*[@class="author"]/a[@class="a-link-normal"]/text()'
         ).get()
-        sample["price"] = response.xpath('//*[@id="price"]/text()').get()
-        sample["publisher"] = response.xpath(
+        book["price"] = response.xpath('//*[@id="price"]/text()').get()
+        book["publisher"] = response.xpath(
             '//*[@id="detailBullets_feature_div"]/ul/li[1]/span/span[2]/text()'
         ).get()
-        sample["release_date"] = response.xpath(
+        book["release_date"] = response.xpath(
             '//*[@id="detailBullets_feature_div"]/ul/li[2]/span/span[2]/text()'
         ).get()
-        sample["isbn10"] = response.xpath(
+        book["isbn10"] = response.xpath(
             '//*[@id="detailBullets_feature_div"]/ul/li[5]/span/span[2]/text()'
         ).get()
-        print("================", sample, "================")
-        yield sample
+        print("================", book, "================")
+        yield book
