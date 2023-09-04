3c268d05e003a1cc40a3de48a2f25411af09a1e0
kinari321 <abcdream321@yahoo.co.jp>
2023-09-04 23:17:37 +0900

    amazon欲しい物リストから取得

--- amazon_wishlist/amazon_wishlist/spiders/amazon.py
+++ amazon_wishlist/amazon_wishlist/spiders/amazon.py
@@ -12,18 +12,30 @@ class AmazonSpider(scrapy.Spider):
     ]
 
     def parse(self, response):
-        book = AmazonWishlistItem()
+        books = response.xpath('//*[@class="a-fixed-right-grid"]')
+        
+        for book_elem in books:
+            book = AmazonWishlistItem()
+            book["title"] = book_elem.xpath('.//a[@class="a-link-normal"]/@title').get()
+            book["author"] = book_elem.xpath('.//span[@class="a-size-base"]/text()').get().strip()
+            book["price"] = response.xpath('.//span[@class="a-price-whole"]/text()').get()
+            book["publisher"] = ""
+            # response.xpath('//*[@id="detailBullets_feature_div"]/ul/li[1]/span/span[2]/text()').get()
+            book["review_star"] = response.xpath('.//span[@class="a-icon-alt"]/text()').get()
+            
+            book["release_date"] = ""
+            # response.xpath('//*[@id="detailBullets_feature_div"]/ul/li[2]/span/span[2]/text()').get()
+            book["isbn10"] = ""
+            # response.xpath('//*[@id="detailBullets_feature_div"]/ul/li[5]/span/span[2]/text()').get()
+            
+            yield {
+                "title": book["title"],
+                "author": book["author"],
+                "price": book["price"],
+                "publisher": book["publisher"],
+                "review_star": book["review_star"],
+                "release_date": book["release_date"],
+                "isbn10": book["isbn10"],
+            }
 
-        book["title"] = response.xpath('//*[@class="a-row a-size-small"]//a[@class="a-link-normal"]/@title').getall()
-        book["price"] = response.xpath('//*[@id="price"]/text()').get()
-        book["publisher"] = response.xpath(
-            '//*[@id="detailBullets_feature_div"]/ul/li[1]/span/span[2]/text()'
-        ).get()
-        book["release_date"] = response.xpath(
-            '//*[@id="detailBullets_feature_div"]/ul/li[2]/span/span[2]/text()'
-        ).get()
-        book["isbn10"] = response.xpath(
-            '//*[@id="detailBullets_feature_div"]/ul/li[5]/span/span[2]/text()'
-        ).get()
-        print("================", book, "================")
-        yield book
+            print("================", book, "================")
